5c551d740e499ffd60398f70431cc4ad75ee2099
Faizudeen Kajogbola <faizudeen.kajogbola@cloudreach.com>
2022-11-18 11:15:15 +0100

    Push back online users via WebSocket

--- src/business/online.go
+++ src/business/online.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 
 	"github.com/Faaizz/simple_http_chatapp/db"
+	"github.com/Faaizz/simple_http_chatapp/msg"
 	"github.com/Faaizz/simple_http_chatapp/types"
 )
 
@@ -42,6 +43,27 @@ func OnlineHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		logger.Errorln(err)
 		msg := "could not find users"
+		logger.Errorln(msg)
+		w.WriteHeader(400)
+		fmt.Fprint(w, msg)
+		return
+	}
+	logger.Debugf("response body: \n%v", users)
+
+	// push message to client via WebSocket
+	uStr, err := json.Marshal(users)
+	if err != nil {
+		logger.Errorln(err)
+		msg := "could not push message via WebSocket. unable to marshal response"
+		logger.Errorln(msg)
+		w.WriteHeader(400)
+		fmt.Fprint(w, msg)
+		return
+	}
+	err = msg.Message(u.ConnectionID, string(uStr), u.Username)
+	if err != nil {
+		logger.Errorln(err)
+		msg := "could not push message via WebSocket"
 		logger.Errorln(msg)
 		w.WriteHeader(400)
 		fmt.Fprint(w, msg)
