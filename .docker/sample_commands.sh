4f5ff41b3fe15a15bdd3063205d8f0aa3a169414
Faizudeen Kajogbola <faizudeen.kajogbola@cloudreach.com>
2022-11-21 13:38:06 +0100

    update with command structure

--- .docker/sample_commands.sh
+++ .docker/sample_commands.sh
@@ -5,8 +5,8 @@ curl http://$HOST_ADDR:$HOST_PORT/connect -d '{"connectionId": "91123456"}' --he
 # set username
 curl http://$HOST_ADDR:$HOST_PORT/username -d '{"connectionId": "91123456", "username": "Asiat"}' --header "Content-Type:application/json"
 # disconnect
-curl http://$HOST_ADDR:$HOST_PORT/disconnect -d '{"connectionId": "91123456", "username": "Aishatutu"}' --header "Content-Type:application/json"
+curl http://$HOST_ADDR:$HOST_PORT/disconnect -d '{"connectionId": "91123456"}' --header "Content-Type:application/json"
 # list users
-curl http://$HOST_ADDR:$HOST_PORT/online -d '{"connectionId": "91123456", "username": "Aishatutu"}' --header "Content-Type:application/json"
+curl http://$HOST_ADDR:$HOST_PORT/online -d '{"connectionId": "91123456"}' --header "Content-Type:application/json"
 # message
-curl http://localhost:$HOST_PORT/message -d '{"connectionId": "91123456", "from_username": "Aishatu"}' --header "Content-Type:application/json"
+curl http://localhost:$HOST_PORT/message -d '{"connectionId": "911234567", "username": "Asiat33", "message": "Hello WebSocket", "url": ""}' --header "Content-Type:application/json"
